f69b8fb38a1e8c615832bc8f34112ca5adba5cfe
devGabyAlves <gabriellyalves401@gmail.com>
2024-09-26 16:14:09 -0300

    feat: implementation of the Calculator1 class for handling and validating requests

--- src/main/routes/calculators.py
+++ src/main/routes/calculators.py
@@ -1,7 +1,13 @@
 from flask import Blueprint, jsonify, request
+from src.calculators.calculator_1 import Calculator1
 
 calc_route_bp = Blueprint("calc_routes", __name__)
 
 @calc_route_bp.route("/calculator/1", methods=["POST"])
-def calculator_1():
-    return jsonify({ "success": True }), 200
+def calculator1():
+    calc = Calculator1()
+    calc.calculate(request)
+
+    return jsonify({
+        "success": True,
+        }), 200
