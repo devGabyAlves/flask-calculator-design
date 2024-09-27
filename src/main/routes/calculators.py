1de4ae93986f20f96210dd8bb20d348250c83791
devGabyAlves <gabriellyalves401@gmail.com>
2024-09-26 22:12:43 -0300

    feat: implementing custom error handling

--- src/main/routes/calculators.py
+++ src/main/routes/calculators.py
@@ -3,25 +3,39 @@ from src.main.factories.calculator1_factory import calculator1_factory
 from src.main.factories.calculator2_factory import calculator2_factory
 from src.main.factories.calculator3_factory import calculator3_factory
 
+from src.errors.error_controller import handle_erros
+
 calc_route_bp = Blueprint("calc_routes", __name__)
 
 @calc_route_bp.route("/calculator/1", methods=["POST"])
 def calculator1():
-    calc = calculator1_factory()
-    response = calc.calculate(request)
+    try:
+        calc = calculator1_factory()
+        response = calc.calculate(request)
 
-    return jsonify(response), 200
+        return jsonify(response), 200
+    except Exception as exception:
+        error_response = handle_erros(exception)
+        return jsonify(error_response["body"]), error_response["satus_code"]
 
 @calc_route_bp.route("/calculator/2", methods=["POST"])
 def calculator2():
-    calc = calculator2_factory()
-    response = calc.calculate(request)
+    try:
+        calc = calculator2_factory()
+        response = calc.calculate(request)
 
-    return jsonify(response), 200
+        return jsonify(response), 200
+    except Exception as exception:
+        error_response = handle_erros(exception)
+        return jsonify(error_response["body"]), error_response["satus_code"]
 
 @calc_route_bp.route("/calculator/3", methods=["POST"])
 def calculator3():
-    calc = calculator3_factory()
-    response = calc.calculate(request)
+    try:
+        calc = calculator3_factory()
+        response = calc.calculate(request)
 
-    return jsonify(response), 200
+        return jsonify(response), 200
+    except Exception as exception:
+        error_response = handle_erros(exception)
+        return jsonify(error_response["body"]), error_response["satus_code"]
